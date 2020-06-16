d99cdc0508001c11338428b33daac9b8dbf6fbfb
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-16 09:26:35 +0900

    [add]クラスの追加と弊害

---
+++ dom_review/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+{
+  document.querySelector("button").addEventListener("click", () => {
+    const targetNode = document.getElementById("target");
+
+    // targetNode.className = 'my-color';
+    // 元の名前も追加しないといけない
+    targetNode.className = "my-color my-border";
+  });
+}
