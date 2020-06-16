59a942e8bd04d988d748bd6b017db12a8b9a9243
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-17 07:50:37 +0900

    [add]ラジオボタンの操作

--- dom_review/main.js
+++ dom_review/main.js
@@ -2,10 +2,20 @@
 
 {
   document.querySelector("button").addEventListener("click", () => {
+    const colors = document.querySelectorAll("input");
+    // 値の保持
+    let selectedColor;
+
+    // colorsに対してroop処理
+    colors.forEach((color) => {
+      // 要素がチェックされているか（ラジオぼたん）
+      if (color.checked === true) {
+        selectedColor = color.value;
+      }
+    });
+
     const li = document.createElement("li");
-    const color = document.querySelector("select");
-    
-    li.textContent = `${color.value} - ${color.selectedIndex}`;
+    li.textContent = selectedColor;
     document.querySelector("ul").appendChild(li);
   });
 }
