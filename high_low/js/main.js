60ec6d18c5a58fcbe00d9eae920c1cc168133c87
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-18 09:48:40 +0900

    [add]要素の取得

---
+++ high_low/js/main.js
@@ -0,0 +1,10 @@
+(function() {
+  "use strict";
+
+  var higher = document.getElementById("higher");
+  var lower = document.getElementById("lower");
+  var dealerCard = document.getElementById("dealer_card");
+  var playerCard = document.getElementById("player_card");
+  var wrapper = document.getElementById("wrapper");
+  var result = document.getElementById("result");
+})();
