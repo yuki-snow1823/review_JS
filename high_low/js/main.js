7d1a55ca154528f0d7dd04c82edd697ab906d195
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-19 09:34:29 +0900

    [add]数値の比較関数

--- high_low/js/main.js
+++ high_low/js/main.js
@@ -7,4 +7,51 @@
   var playerCard = document.getElementById("player_card");
   var wrapper = document.getElementById("wrapper");
   var result = document.getElementById("result");
+
+  var dealerValue;
+  var playerValue;
+
+  function getRandom() {
+    return Math.floor(Math.random() * 13 + 1);
+  }
+
+  function init() {
+    dealerValue = getRandom();
+    dealerCard.textContent = dealerValue;
+    playerValue = getRandom();
+    playerCard.textContent = playerValue;
+  }
+
+  function check(guess) {
+    var str;
+    wrapper.classList.add("open");
+    if (playerValue === dealerValue) {
+      str = "draw";
+    } else {
+      str = "You " + getResultStr(guess); // 関数を文字列としてしまう
+    }
+    result.textContent = str;
+    result.classList.remove("hidden");
+  }
+
+  function getResultStr(guess) {
+    if (
+      (playerValue > dealerValue && guess === "higher") ||
+      (playerValue < dealerValue && guess === "lower")
+    ) {
+      return "win!";
+    } else {
+      return "lose...";
+    }
+  }
+
+  init();
+
+  higher.addEventListener("click", function() {
+    check("higher");
+  });
+
+  lower.addEventListener("click", function() {
+    check("lower");
+  });
 })();
